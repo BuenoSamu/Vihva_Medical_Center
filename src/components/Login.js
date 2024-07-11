68cf06b72bef550b009d25fd99c20f5621afc77f
Pedrolbtb <pedro.lbtb@gmail.com>
2024-07-11 14:12:05 -0300

    front end tela de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,13 @@
 // src/components/Login.js
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, BrowserRouter } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from './firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
+import { FaEnvelope } from 'react-icons/fa';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import './Login.css';
+import logo from './vivah_logo_500x.png'
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -38,33 +40,46 @@ const Login = () => {
 
   return (
     <div>
-      <h1 className="title">Vihva</h1>
-      <div className="login-container">
-        <h2>Login</h2>
+      <div className='ContainerBemVindo'>
+      <img src={logo} className='imgLogo'></img>
+      <h1 className='titleBemvindo'>Vihva Medical Center</h1>
+      <p className='slogan'>Elevando o nível do atendimento de clientes dia após dia</p>
+      <p className='slogan'>Caso não tenha conta, crie agora!</p>
+      <button className='naotenhoconta'><Link to="/signup">Criar Conta</Link></button>
+      </div>
+      <h1 className="titlea">Login</h1>
+      
+      <div className="Clogin">
+        <p className='descTitle'>Insira seus dados</p>
         <form onSubmit={handleLogin}>
+          <div className='inputContainer'>
           <input
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="small-input"
+            className="inputLogin"
           />
-          <div className="password-input">
+          <FaEnvelope className="icon" />
+          </div>
+          <div className="inputContainer">
             <input
               type={showPassword ? 'text' : 'password'}
               placeholder="Senha"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="small-input"
+              className="inputLogin"
             />
+            <div className='iconPassword'>
             <span className="toggle-password" onClick={togglePasswordVisibility}>
               {showPassword ? <FiEyeOff /> : <FiEye />}
             </span>
+            </div>
           </div>
           {error && <p>{error}</p>}
-          <button type="submit" className="small-button">Entrar</button>
+          <button type="submit" className="buttonLogin">Entrar</button>
         </form>
-        <p>Não tem uma conta? <Link to="/signup">Clique aqui para Criar Conta</Link></p>
+        <p className='textEsqueciSenha'><u>Esqueci minha senha</u></p>
       </div>
     </div>
   );
